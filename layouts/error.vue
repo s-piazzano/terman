0c9c4b861191bb5ea55026ffbc37ae0414b2f092
Stefano Piazzano <piazzano.stefano@gmail.com>
2020-08-10 18:54:11 +0200

    Add error layout

---
+++ layouts/error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h1 v-if="error.statusCode === 404">Pagina Non trovata</h1>
+        <h1 v-else>Ops... Un errore... :(</h1>
+        <NuxtLink to="/">Ritorna alla Home Page</NuxtLink>
+        <Nuxt />
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['error'],
+    layout: 'error'
+}
+</script>
+
+<style>
+
+</style>
